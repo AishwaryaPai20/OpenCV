bc9e00a605d22bb6ef43cdf02c152277d23e9e53
AishwaryaPai20 <aishwaryampai200203@gmail.com>
2022-05-08 22:43:24 +0530

    BITWISE OPERATIONS

---
+++ bitwise.py
@@ -0,0 +1,30 @@
+# turned on 1 turned off 0
+
+import cv2 as cv
+import numpy as np
+from pyparsing import DictType
+
+blank = np.zeros((400, 400), dtype="uint8")
+rectangle = cv.rectangle(blank.copy(), (30, 30), (370, 370), 255, -1)
+circle = cv.circle(blank.copy(), (200, 200), 200, 255, -1)
+
+cv.imshow('Rectangle', rectangle)
+cv.imshow('Circle', circle)
+
+# Bitwise AND  --> intersecting regions
+bitwise_and = cv.bitwise_and(rectangle, circle)
+cv.imshow('Bitwise AND', bitwise_and)
+
+# Bitwise or --> non intersecting and intersecting regions
+bitwise_or = cv.bitwise_or(rectangle, circle)
+cv.imshow('Bitwise OR', bitwise_or)
+
+# Bitwise xor --> non intersecting regions
+bitwise_xor = cv.bitwise_xor(rectangle, circle)
+cv.imshow('Bitwise XOR', bitwise_xor)
+
+#Bitwise not --> inverse of the image
+bitwise_not = cv.bitwise_not(circle)
+cv.imshow('Bitwise NOT', bitwise_not)
+
+cv.waitKey(0)
